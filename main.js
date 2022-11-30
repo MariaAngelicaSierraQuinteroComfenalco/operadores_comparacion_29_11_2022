6029169f359a388164c7bb15626bfc74d1acb5b3
MariaAngelicaSierraQuinteroComfenalco <angelicasierr904@gmail.com>
2022-11-29 21:29:53 -0500

    Mayor o igual que (>=)

--- main.js
+++ main.js
@@ -1,26 +1,29 @@
 addEventListener("DOMContentLoaded", (e) => {
 
     document.querySelector("body").innerHTML = "Usario porfavor revisar consola (oprimir tecla f12)";
-    document.querySelector("body").innerHTML = "Mayor que (>)";
+    document.querySelector("body").innerHTML = "Mayor o igual que (>=)";
     document.querySelector("body").innerHTML = "la variable num1= 3  y num2= 4";
 
-    //el operador de comparacion de mayor que se representa  >  en este nos dira si es mayor que el otro numero 
+    //el operador de comparacion de mayor o igual que se representa  >=  en este nos dira si es mayor o igual que el otro numero 
     // su respuesta sera true o false 
-
+    //sin importar el tipo 
     let num1 = 3;//variables
     let num2 = 4;
   
-    console.group("¿ num1 es mayor que num2"); //se realiza contexto de lo preguntamos
-        console.log(num1 > num2);  //se utiliza el operador
+    console.group("¿ num1 es mayor o igual que num2"); //se realiza contexto de lo preguntamos
+        console.log(num1 >= num2);  //se utiliza el operador
     console.groupEnd();
     
-    console.group("¿ num2 es mayor que num1 ?");
-        console.log(num2>num1);
+    console.group("¿ num2 es mayor o igual que num1 ?");
+        console.log(num2>=num1);
     console.groupEnd();
     
-    console.group("¿ '12' es mayor que 2?");
-        console.log("12" > 2);  
+    console.group("¿ '2' es mayor o igual que 2?");
+        console.log("2" >= 2);  
     console.groupEnd();
 
+    console.group("¿ num1 es mayor o igual 3 ?");
+        console.log(num1>=3);
+    console.groupEnd();
 
 })
