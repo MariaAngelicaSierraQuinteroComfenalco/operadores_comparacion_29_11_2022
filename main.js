9807280e73671f5ce20fee85b565d50f7aa1fe3f
MariaAngelicaSierraQuinteroComfenalco <angelicasierr904@gmail.com>
2022-11-29 20:30:39 -0500

    Estrictamente igual (===)

--- main.js
+++ main.js
@@ -1,23 +1,22 @@
 addEventListener("DOMContentLoaded", (e) => {
 
     document.querySelector("body").innerHTML = "Usario porfavor revisar consola (oprimir tecla f12)";
-    document.querySelector("body").innerHTML = "No es igual (!=)";
+    document.querySelector("body").innerHTML = "Estrictamente igual (===)";
     document.querySelector("body").innerHTML = "la variable num1= 3  y num2= 4";
 
-    //el operador de comparacion no es igual o o tambien conocido como diferente  se representa != en este nos dira si es diferente
+    //el operador de comparacion Estrictamente igual se representa === en este nos dira si es igual si los operadores son iguales y del mismo tipo
+    //si no cumple los dos no se puede entonces saldra false como en el ejemplo
     // su respuesta sera true o false 
 
     let num1 = 3;//variables
     let num2 = 4;
-    console.group("¿ num1 no es igual a 4?"); //se realiza contexto de lo preguntamos
-    console.log(num1 != 4);  //se utiliza el operador
-    console.group("¿ num2 no es igual a '3' ?");
-    console.log(num2 != "3");
+    console.group("¿ num1 es igual estrictamente a 3?"); //se realiza contexto de lo preguntamos
+    console.log(3 === num1);  //se utiliza el operador
+    console.group("¿ num1 es igual estrictamente a '3' ?");
+    console.log(num1 === "3");
+    console.log("al ser estrictamente debe considir en tipo de dato y operandos ");
     console.groupEnd();
-    console.group("¿num1 no es igual a 3?");
-    console.log(num1 != 3);
-    console.groupEnd();
-   
+
 
 
 })
