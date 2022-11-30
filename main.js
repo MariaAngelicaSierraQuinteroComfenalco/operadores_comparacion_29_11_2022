9ee3ce360b147b1ed0a22257137cbd13d495ffbb
MariaAngelicaSierraQuinteroComfenalco <angelicasierr904@gmail.com>
2022-11-29 21:19:57 -0500

    Mayor que (>)

--- main.js
+++ main.js
@@ -1,25 +1,25 @@
 addEventListener("DOMContentLoaded", (e) => {
 
     document.querySelector("body").innerHTML = "Usario porfavor revisar consola (oprimir tecla f12)";
-    document.querySelector("body").innerHTML = "Desigualdad estricta (!==)";
+    document.querySelector("body").innerHTML = "Mayor que (>)";
     document.querySelector("body").innerHTML = "la variable num1= 3  y num2= 4";
 
-    //el operador de comparacion de desigualdad estricta se representa !== en este nos dira si no es igual pero cumpliendo dos condiciones
-    // los operadores son iguales y del mismo tipo
-    //si no cumple los dos no se puede entonces saldra false 
+    //el operador de comparacion de mayor que se representa  >  en este nos dira si es mayor que el otro numero 
     // su respuesta sera true o false 
 
     let num1 = 3;//variables
     let num2 = 4;
-    console.log("debe considir en tipo de dato y operandos ");
-    console.group("¿ num1 no es igual estrictamente a '3'?"); //se realiza contexto de lo preguntamos
-    console.log(num1 !== "3");  //se utiliza el operador
+  
+    console.group("¿ num1 es mayor que num2"); //se realiza contexto de lo preguntamos
+        console.log(num1 > num2);  //se utiliza el operador
     console.groupEnd();
-    console.group("¿ 3 no es igual a '3' ?");
-    console.log(3 !== '3');
+    
+    console.group("¿ num2 es mayor que num1 ?");
+        console.log(num2>num1);
     console.groupEnd();
-    console.group("¿ num2 no es igual estrictamente a 4?");
-    console.log(num2 !== 4);  
+    
+    console.group("¿ '12' es mayor que 2?");
+        console.log("12" > 2);  
     console.groupEnd();
 
 
